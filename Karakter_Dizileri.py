2e48f957c8d04eef68ceb51135fefbfcc42d0302
Bertuğ <73167951+bertuginal@users.noreply.github.com>
2021-05-08 23:52:11 +0300

    Add files via upload

---
+++ Karakter_Dizileri.py
@@ -0,0 +1,12 @@
+isim = 'Bertuğ'
+soyad = 'İNAL'
+yas = 20
+memleket = "İzmir"
+meslek = "Mühendis"
+tanitim = "Benim adım " + isim + " Soyadım " + soyad + " Yaşım "+ str(yas) + " Memleketim " + memleket + " Mesleğim " + meslek
+print(tanitim[-1])
+print(tanitim[2:7])
+print(len(tanitim))
+print(tanitim[2:])
+print(tanitim[:20])
+print(tanitim[4:25:3])
