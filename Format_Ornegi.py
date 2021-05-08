572228041b34b162e88120c407e7096b36489e10
Bertuğ <73167951+bertuginal@users.noreply.github.com>
2021-05-08 23:48:44 +0300

    Add files via upload

---
+++ Format_Ornegi.py
@@ -0,0 +1,13 @@
+isim = "Bertuğ"
+soyad = 'İnal'
+meslek = "Bilgisayar mühendisiyim"
+print('İsmim {} ' .format(isim))
+print("ismim {1} {0} ".format(isim,soyad))
+
+print("Benim adım {i} {s}".format(i = isim,s=soyad))
+print("İsmim {} {} ve ben {} ".format(isim,soyad,'mühendisim'))
+deger = 22/7
+print("İşlemin sonucu = {d:6.4} " .format(d=deger))
+
+
+print(f"İsmim {isim} {soyad} ve ben {'mühendisim'} ")
