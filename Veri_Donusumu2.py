314156afde211698951eddff216f8d6690eee51e
Bertuğ <73167951+bertuginal@users.noreply.github.com>
2021-05-09 00:01:57 +0300

    Add files via upload

---
+++ Veri_Donusumu2.py
@@ -0,0 +1,8 @@
+r = float(input('Yarıçap : '))
+pi = 3.14159
+cevre =2*pi*r
+alan = pi*(r**2)
+print('Çevre : ',cevre)
+print('Alan : ', int(alan))
+print('Çevre:'+str(cevre)+'Alan:'+str(alan))
+print('Çevre:',cevre,'Alan:',alan)
