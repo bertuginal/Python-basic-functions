5cce09a6563bb292013c40aecf07bd9b8dc9186b
Bertuğ <73167951+bertuginal@users.noreply.github.com>
2021-05-09 00:01:36 +0300

    Add files via upload

---
+++ Veri_Donusumu.py
@@ -0,0 +1,50 @@
+a = input('İlk sayı : ')
+b = input('İkinci sayı : ')
+c = input('Üçüncü sayı : ')
+
+print(type(a))
+print(type(b))
+print(type(c))
+
+toplam = int(a)+int(b)+int(c)
+print('Toplam : ', toplam)
+
+
+
+a=int(a)
+b=float(b)
+c=int(c)
+
+print(a)
+print(b)
+print(c)
+print(type(a))
+print(type(b))
+print(type(c))
+
+toplam = a+b+c
+print(toplam)
+
+sonuc = a+b+c
+
+print(str(sonuc))
+
+
+hesabiVar=False
+hesabiVar=str(hesabiVar)
+print(hesabiVar)
+
+hesabiVar=int(hesabiVar)
+print(hesabiVar)
+
+
+a=3.2
+b=4
+c=7.3
+
+sonuc = a + b + c
+sonuc = int(sonuc)
+print(sonuc)
+sonuc = float(sonuc)
+print(sonuc)
+
