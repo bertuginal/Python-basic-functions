70bfea8ecdd16811c1e23b20afa1c531dfca7931
Bertuğ <73167951+bertuginal@users.noreply.github.com>
2021-05-08 23:40:02 +0300

    Add files via upload

---
+++ String_uzunluk.py
@@ -0,0 +1,15 @@
+websitem = 'https://github.com/bertuginal'
+kurs = 'Hedefiniz Akdeniz! İleri...'
+
+sonuc = len(websitem)
+print(sonuc)
+
+sonuc2 = kurs[0:8]
+print(sonuc2)
+
+length = len(kurs)
+print(length)
+sonuc3 = kurs[length-5:length]
+print(sonuc3)
+sonuc4 = kurs[-5:]
+print(sonuc4)
